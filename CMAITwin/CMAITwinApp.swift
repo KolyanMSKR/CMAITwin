28f3892d4993b56b0e58fda676d6fb4f25759ba3
Anderen <kolyanmskr@gmail.com>
2025-06-22 11:56:18 +0300

    Initial Commit

---
+++ CMAITwin/CMAITwinApp.swift
@@ -0,0 +1,17 @@
+//
+//  CMAITwinApp.swift
+//  CMAITwin
+//
+//  Created by Anderen on 22.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct CMAITwinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
